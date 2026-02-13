bf1dc7ce88ed309a2a9deb936a17b63bcd34ca98
kimsrik <rltmrl2@gmail.com>
2026-02-13 11:27:52 +0900

    back // RootConfig Update

--- back/src/main/java/com/example/demo/config/RootConfig.java
+++ back/src/main/java/com/example/demo/config/RootConfig.java
@@ -3,11 +3,13 @@ package com.example.demo.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RootConfig {
 	
+	@Bean
 	public ModelMapper getMapper() {
 		ModelMapper modelMapper = new ModelMapper();
 		
