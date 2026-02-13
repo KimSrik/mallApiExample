1f2f5acdd60000b0e47c2ddfd2d4d7f858b4129f
kimsrik <rltmrl2@gmail.com>
2026-02-13 16:45:29 +0900

    back // modify, delete added

--- back/src/main/java/com/example/demo/controller/TodoController.java
+++ back/src/main/java/com/example/demo/controller/TodoController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,26 @@ public class TodoController {
 		Long tno = service.register(todoDTO);
 		
 		return Map.of("TNO", tno);
+	}
+	
+	@PutMapping("/{tno}")
+	public Map<String, String> modify(@PathVariable(name="tno") Long tno, @RequestBody TodoDTO todoDTO){
+		todoDTO.setTno(tno);
+		
+		log.info("Modify : " + todoDTO);
+		
+		service.modify(todoDTO);
+		
+		return Map.of("RESULT", "SUCCESS");
+	}
+	
+	@DeleteMapping("/{tno}")
+	public Map<String, String> remove(@PathVariable(name="tno") Long tno) {
+		log.info("Remove : " + tno);
+		
+		service.remove(tno);
+		
+		return Map.of("RESULT", "SUCCESS");
 	}
 	
 
