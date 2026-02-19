f8ef327b14bf5f1478bb61b03120c00c5c7be706
kimsrik <rltmrl2@gmail.com>
2026-02-19 12:55:16 +0900

    front // ReadPage modify

--- front/src/pages/todo/ReadPage.jsx
+++ front/src/pages/todo/ReadPage.jsx
@@ -5,30 +5,6 @@ import ReadComponent from "../../components/todo/ReadComponent";
 const ReadPage = () => {
 
   const {tno} = useParams();
-
-  const navigate = useNavigate();
-
-  const [queryParams] = useSearchParams();
-
-  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1 ;
-  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10 ;
-
-  const queryStr = createSearchParams({page, size}).toString();
-
-  const moveToModify = useCallback( (tno) => {
-    navigate({
-      pathname : `/todo/modify/${tno}`,
-      search : queryStr
-    })
-  }, [tno, page, size]);
-  // }, [tno, queryParams, navigate]);
-
-  const moveToList = useCallback( () => {
-    navigate({
-      pathname : '/todo/list',
-      search:querystr
-    })
-  }, [page, size] );
   
 
   return (
