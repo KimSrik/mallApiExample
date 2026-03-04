9f9a1e71f6a54ad59c7fd91af66945d72335c57e
kimsrik <rltmrl2@gmail.com>
2026-03-04 12:55:35 +0900

    front // root -> productsRouter regist

--- front/src/router/root.jsx
+++ front/src/router/root.jsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import todoRouter from "./todoRouter";
+import productsRouter from "./productsRouter";
 
 const Loading = <div>Loading...</div>;
 
@@ -10,7 +11,8 @@ const Loading = <div>Loading...</div>;
 const Main = lazy( () => import("../pages/MainPage") );
 const About = lazy( () => import("../pages/AboutPage") );
 const TodoIndex = lazy ( () => import("../pages/todo/IndexPage") );
-const TodoList = lazy ( ()=> import("../pages/todo/ListPage") );
+
+const ProductsIndex = lazy ( () => import("../pages/products/IndexPage") );
 
 
 
@@ -27,6 +29,11 @@ const root = createBrowserRouter([
         path:"todo",
         element:<Suspense fallback={Loading}><TodoIndex></TodoIndex></Suspense>,
         children:todoRouter()
+    },
+    {
+        path:"products",
+        element:<Suspense fallback={Loading}><ProductsIndex></ProductsIndex></Suspense>,
+        children:productsRouter()
     }
 
 ]);
