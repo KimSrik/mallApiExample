fb899487413914731668529289b935166dd6aceb
kimsrik <rltmrl2@gmail.com>
2026-03-04 12:18:20 +0900

    back // controller - modify() added

--- back/src/main/java/com/example/demo/controller/ProductController.java
+++ back/src/main/java/com/example/demo/controller/ProductController.java
@@ -2,12 +2,14 @@ package com.example.demo.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -60,6 +62,52 @@ public class ProductController {
 		log.info("list ---------------------");
 		
 		return productService.getList(pageRequestDTO);
+	}
+	
+	@GetMapping("/{pno}")
+	public ProductDTO read(@PathVariable(name="pno") Long pno) {
+		return productService.get(pno);
+	}
+	
+	@PutMapping("/{pno}")
+	public Map<String, String> modify(@PathVariable(name="pno") Long pno, ProductDTO productDTO) {
+		
+		productDTO.setPno(pno);
+		
+		ProductDTO oldProductDTO = productService.get(pno);
+		// 기존의 파일들(데이터베이스에 존재하는 파일 - 수정과정에서 삭제되었을수도 있음)
+		List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+		
+		// 새로 업로드 해야하는 파일들
+		List<MultipartFile> files = productDTO.getFiles();
+		
+		// 새로 업로드되서 만들어진 파일 이름들
+		List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+		
+		// 화면에서 변화없이 계속 유지된 파일들
+		List<String> uploadFileNames = productDTO.getUploadFileNames();
+		
+		// 유지되는 파일들 + 새로 업로드된 파일 이름들이 저장해야하는 파일 목록이 됨
+		if(currentUploadFileNames != null && currentUploadFileNames.size() > 0 ) {
+			uploadFileNames.addAll(currentUploadFileNames);
+		}
+		
+		// 수정 작업(service 접근)
+		productService.modify(productDTO);
+		
+		if(oldFileNames != null && oldFileNames.size() > 0) {
+			// 지워야 하는 파일 목록 찾기
+			// 예전 파일들 중에서 지워져야 하는 파일 이름들
+			List<String> removeFiles = oldFileNames
+					.stream()
+					.filter(fileName -> uploadFileNames.indexOf(fileName) == -1)
+					.collect(Collectors.toList());
+			
+			// 실제 파일 삭제 작업
+			fileUtil.deleteFiles(removeFiles);
+		}
+		
+		return Map.of("RESULT", "SUCCESS");
 	}
 	
 }
