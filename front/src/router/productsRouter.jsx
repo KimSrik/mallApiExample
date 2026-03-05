8e930d611745e2013040eb184c3d0ba1262b072d
kimsrik <rltmrl2@gmail.com>
2026-03-05 10:51:32 +0900

    front // productRouter list router setting

--- front/src/router/productsRouter.jsx
+++ front/src/router/productsRouter.jsx
@@ -1,6 +1,15 @@
+import { Suspense, lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+const Loading = <div>Loading......</div>
+const ProductsList = lazy ( () => import("../pages/products/ListPage") );
+
 const productsRouter = () => {
   return[
-
+    {
+      path:"list",
+      element:<Suspense fallback={Loading}><ProductsList></ProductsList></Suspense>
+    }
   ]
 }
 export default productsRouter;
