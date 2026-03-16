d7c8f9aeef95d1a79b491d5603989b4055377690
kimsrik <rltmrl2@gmail.com>
2026-03-16 10:12:16 +0900

    front // main.jsx added

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDom from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+import { Provider } from "react-redux"
+import store from './store.js'
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+    <Provider stroe={store}>
+        <App></App>
+    </Provider>
 )
 
 // <strictmode>를 삭제함으로써 두번씩 실행되는 것을 방지함
