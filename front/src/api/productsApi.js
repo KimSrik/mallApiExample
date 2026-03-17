a7b7690d73d0a5d7234128d06494850af5f6d2eb
KimSrik <rltmrl2@gmail.com>
2026-03-17 12:05:14 +0900

    front // api import fix

--- front/src/api/productsApi.js
+++ front/src/api/productsApi.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+import jwtAxios from "../util/jwtUtil";
+
 import { API_SERVER_HOST } from "./todoApi";
 
 const host = `${API_SERVER_HOST}/api/products`;
@@ -7,7 +8,7 @@ export const postAdd = async (product) => {
   
   const header = {headers:{"Content-Type" : "multipart/form-data"}}
 
-  const res = await axios.post(`${host}/`, product, header)
+  const res = await jwtAxios.post(`${host}/`, product, header)
 
   return res.data;
 }
@@ -15,13 +16,13 @@ export const postAdd = async (product) => {
 export const getList = async (pageParam) => {
   const {page, size} = pageParam;
 
-  const res = await axios.get(`${host}/list`, {params:{page:page, size:size}});
+  const res = await jwtAxios.get(`${host}/list`, {params:{page:page, size:size}});
 
   return res.data;
 }
 
 export const getOne = async (pno) => {
-  const res = await axios.get(`${host}/${pno}`);
+  const res = await jwtAxios.get(`${host}/${pno}`);
 
   return res.data;
 }
@@ -30,13 +31,13 @@ export const putOne = async (pno, product) => {
 
   const header = { headers:{"Content-Type" : "multipart/form-data"} }
 
-  const res = await axios.put(`${host}/${pno}`, product, header);
+  const res = await jwtAxios.put(`${host}/${pno}`, product, header);
 
   return res.data;
 }
 
 export const deleteOne = async (pno) => {
-  const res = await axios.delete(`${host}/${pno}`);
+  const res = await jwtAxios.delete(`${host}/${pno}`);
 
   return res.data;
 }
