4464293f500fbc1e05a2a8475e42266e3e5c007f
KimSrik <rltmrl2@gmail.com>
2026-03-17 12:57:36 +0900

    front // refresh access token added

--- front/src/util/jwtUtil.jsx
+++ front/src/util/jwtUtil.jsx
@@ -1,7 +1,23 @@
 import axios from "axios";
-import { getCookie } from "./cookieUtil";
+import { getCookie, setCookie } from "./cookieUtil";
+import { API_SERVER_HOST } from "../api/todoApi";
 
 const jwtAxios = axios.create();
+
+const refreshJWT = async (accessToken, refreshToken) => {
+
+  const host = API_SERVER_HOST;
+
+  const header = {headers:{"Authorization":`Bearer ${accessToken}`}};
+
+  const res = await axios.get(`${host}/api/member/refresh?refreshToken=${refreshToken}`, header);
+
+  console.log("----------------------------");
+  console.log(res.data);
+
+  return res.data;
+
+}
 
 const beforeReq = (config) => {
   console.log("before request ..........");
@@ -36,6 +52,29 @@ const requestFail = (err) => {
 
 const beforeRes = async (res) => {
   console.log("before return Response ..........");
+
+  console.log("----------------------------")
+  console.log("백엔드에서 온 데이터 : " , res);
+  console.log("----------------------------")
+
+  const data = res.data;
+
+  if(data && data.error === 'ERROR_ACCESS_TOKEN'){
+    const memberCookieValue = getCookie("member");
+
+    const result = await refreshJWT(memberCookieValue.accessToken, memberCookieValue.refreshToken);
+
+    console.log("refreshJWT RESULT", result);
+
+    memberCookieValue.accessToken = result.accessToken;
+    memberCookieValue.refreshToken = result.refreshToken;
+
+    setCookie("member", JSON.stringify(memberCookieValue), 1);
+
+    const originalRequest = res.config;
+
+    originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+  }
 
   return res;
 }
